19ec63414f451f59e9093b1ca99cdd229093e777
Max16 <max16@naver.com>
2021-05-02 20:14:25 +0900

    #3.3 GlobalStyles and Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import Router from "Components/Router";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <>
       <Router />
+      <GlobalStyles />
     </>
   );
 }
