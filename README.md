d7db592276f7011d9e4bbe4d6d21063a1746c20f
Max16 <max16@naver.com>
2021-05-02 22:20:00 +0900

    #4.3 API Verbs part Two

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Movie app by React JS
   - [ ] Tv Shows
   - [ ] Search
   - [ ] Detail
+
+## API Verbs
+
+ - [x] Now Playing(Movie)
+ - [x] Upcoming(Movie)
+ - [x] Top Rated(TV)
+ - [x] Popular(TV, Movie)
+ - [x] Airing Today(TV)
+ - [x] TV Show Detail
+ - [x] Movie Detail
+ - [x] Search (Movie, TV)
---
+++ src/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "83e4562556a8e370915aa2a360e7d4db",
+    language: "kr-KR",
+  },
+});
+
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  serach: (term) =>
+    api.get("serach/movie", {
+      params: {
+        query: term,
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  serach: (term) =>
+    api.get("serach/tv", {
+      params: {
+        query: term,
+      },
+    }),
+};
