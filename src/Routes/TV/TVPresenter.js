9f80ecd4be0b0a884fc3639c49811c42b73c404b
Max16 <max16@naver.com>
2021-05-03 15:39:19 +0900

    #6.1 HomePresenter and Section Component

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -2,9 +2,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div``;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated">
+          {topRated.map((movie) => movie.name)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular">{popular.map((movie) => movie.name)}</Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today">
+          {airingToday.map((movie) => movie.name)}
+        </Section>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTyles = {
   topRated: PropTypes.array,
